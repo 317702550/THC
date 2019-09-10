16ab3f1cd8d475acb9111a40ded26cd09d906a49
Maximiliano Proaño Bernal <maxpbernal@ciencias.unam.mx>
2019-09-10 14:58:51 -0500

    Se agregaron pelota01 y pelota02

---
+++ Python/pelota01.py
@@ -0,0 +1,12 @@
+#
+# Maximiliano Proaño Bernal
+# Python 3.7.3
+# 10/09/19
+# 12/09/19
+#
+# Calcula la posición de una pelota con una velocidad inicial
+# de 5 m/s y un tiempo de 0.6 segundos
+print((5*0.6)-(0.5*9.8*0.6**2))
+# write((5*0.6)-(0.5*9.8*0.6**2)) write no es una palabra reservada y no aparece en morado
+
+
---
+++ Python/pelota02.py
@@ -0,0 +1,15 @@
+#
+# Maximiliano Proaño Bernal
+# Python 3.7.3
+# 10/09/19
+# 12/09/19
+#
+# Calcula la posición de una pelota con una velocidad inicial
+# de 5 m/s y un tiempo de 0.6 segundos
+v0 = 5
+g = 9.81
+t = 0.6
+y = v0*t-0.5*g*t**2
+print(y)
+
+
