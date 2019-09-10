17eaafacec3cb2e3f936008c16a85f30fcdf703c
Maximiliano Proaño Bernal <maxpbernal@ciencias.unam.mx>
2019-09-10 17:34:50 -0500

    Se agregó el problema a escoger

---
+++ Python/Problema/Problema01.py
@@ -0,0 +1,12 @@
+#
+# Maximiliano Proaño Bernal
+# Python 3.7.3
+# 10/09/19
+# 12/09/19
+#
+# Calcula la energía cinética de un tren con masa 500
+# toneladas y velocidad 80 m/s
+v0 = 80
+m = 500000
+Ec = (m*v0**2)/2
+print(Ec)
