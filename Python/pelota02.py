1f1a18841d9d36e61415f2460821de06c2165877
Maximiliano Proaño Bernal <maxpbernal@ciencias.unam.mx>
2019-09-12 14:55:49 -0500

    Se modificó pelota02 y se agregó pelota03

--- Python/pelota02.py
+++ Python/pelota02.py
@@ -11,5 +11,9 @@ g = 9.81
 t = 0.6
 y = v0*t-0.5*g*t**2
 print(y)
-
+print("En el tiempo t=%g segundos, la altura de la pelota es %.2f metros"%(t,y))
+print("""En el tiempo t=%g segundos, la altura de \nla pelota es %.2f metros"""%(t,y))
+print("""
+En el tiempo t=%g segundos, la pelota con velocidad=%.3E m/s, la altura de
+la pelota es %.2f metros"""%(t,v0,y))
 
