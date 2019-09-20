708ed9839b76fe043ac4c99dc5959ac40d8923ed
Maximiliano Proaño Bernal <maxpbernal@ciencias.unam.mx>
2019-09-20 08:34:39 -0500

    Se agregó el ejercicio 01

---
+++ Python/Ejercicio01.py
@@ -0,0 +1,66 @@
+# Maximiliano Proaño Bernal
+# Python 3.7.3
+# 19/09/12
+# 24/09/12
+# Ejercicio 01
+
+2 + 2 # Realiza y muestra el reslutado de la suma de 2 más 2
+50 - 5*6 # Realiza y muestra el reslutado de la resta de 50 menos el producto de 5 por 6
+(50 - 5*6) / 4 # Realiza y muestra el reslutado de la división de la resta de 50 menos el producto de 5 por 6 entre 4
+8 / 5 # Realiza y muestra el resultado de dividir 8 entre 5
+
+17 / 3 # Realiza y muestra el resultado de dividir 17 entre 3
+17 // 3 # Realiza y muestra el resultado del mayor entero menor o igual al dividir 17 entre 3
+17 % 3 # Realiza la divisón 17 entre 3 y muestra el residuo de la división
+5 * 3 + 2 # Realiza y muestra el resultado de la suma de 2 más el producto de 5 por 3
+
+5 ** 2 #
+2 ** 7 #
+9 ** 0.5 #
+2 ** 1/2 #
+2 ** (1/2) #
+
+ancho = 20 #
+alto = 5 * 9 #
+ancho * alto #
+
+n #
+
+4 * 3.75 - 1 #
+
+iva = 16/100 #
+precio = 120.5 #
+precio * iva #
+precio + _ #
+round(_, 2) #
+
+# Cadenas
+'una cadena' #
+'La comilla simple \'' #
+'La comilla simple \'' #
+"\"Si,\" afirmo" #
+
+"\"Si,\" afirmo" #
+print("\"Si,\" afirmo") #
+
+c = 'Primera linea.\nSegunda linea.' #
+c #
+print(c) #
+len(c) #
+
+print('Una ruta en windows C:\algun\nombre') #
+print(r'Una ruta en windows C:\algun\nombre') #
+
+
+print("""\
+Uso: ssh [OPCIONES] <usuario>@<servidor>
+     -v                 muestra informacion adiciona de la conexion
+     -p puerto          Puerto para la conexion segura, 22 es el predeterminado
+""") #
+
+2 * 'goya ' + 'cachun' #
+
+'Py' 'thon' #
+
+text = ('Escribe varias cadenas dentro del par´entesis '
+        'para tenerlas unidas') #
