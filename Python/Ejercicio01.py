95bfca185492d2e222c3d2cce8882ab5451abadd
Maximiliano Proaño Bernal <maxpbernal@ciencias.unam.mx>
2019-09-23 20:20:00 -0500

    Se modificó Ejercicio01

--- Python/Ejercicio01.py
+++ Python/Ejercicio01.py
@@ -35,68 +35,78 @@ precio + _ # Realiza y muestra el resultado de sumar precio más el precio por e
 round(_, 2) # Redondea el resultado anterior a dos decimales
 
 # Cadenas
-'una cadena' #
-'La comilla simple \'' #
-'La comilla simple \'' #
-"\"Si,\" afirmo" #
+'una cadena' # Imprime entre comillas simples "una cadena"
+'La comilla simple \'' # Imprime entre comillas simples "La comilla simple'"
+"\"Si,\" afirmo" # Imprime entre comillas simples " "Si", afirmo "
+print("\"Si,\" afirmo") # Imprime " "Si", afrimo "
 
-"\"Si,\" afirmo" #
-print("\"Si,\" afirmo") #
+c = 'Primera linea.\nSegunda linea.' # Guarda "c" como "Primer Linea (salto de renglón) Segunda linea" sin respetar los saltos de lineas
+c # Muestra el valor entrecomillado de "c"
+print(c) # Imprime la variable "c" con salto de línea debido al \n
+len(c) # Muestra el número de caracteres usados, incluyendo espacios
 
-c = 'Primera linea.\nSegunda linea.' #
-c #
-print(c) #
-len(c) #
-
-print('Una ruta en windows C:\algun\nombre') #
-print(r'Una ruta en windows C:\algun\nombre') #
+print('Una ruta en windows C:\algun\nombre') # Imprime "Una ruta en windows C:lgun ombre" (/a se representa como  y /n como un salto de linea).
+print(r'Una ruta en windows C:\algun\nombre') # Imprime "Una ruta en windows C:\algun\nombre" (r nulifica las propiedades de /a y /n).
 
 
 print("""\
 Uso: ssh [OPCIONES] <usuario>@<servidor>
      -v                 muestra informacion adiciona de la conexion
      -p puerto          Puerto para la conexion segura, 22 es el predeterminado
-""") #
+""") # Imprime lo que se encuentra dentro de las comillas tal cual está 
 
-2 * 'goya ' + 'cachun' #
+2 * 'goya ' + 'cachun' # Imprime 2 veces "goya" y agrega una vez "cachun"
 
-'Py' 'thon' #
+'Py' 'thon' # Imprime "Python" todo junto
 
-text = ('Escribe varias cadenas dentro del par´entesis '
-        'para tenerlas unidas') #
+text = ('Escribe varias cadenas dentro del paréntesis '
+        'para tenerlas unidas') # Guarda "text" como 'Escribe varias cadenas dentro del paréntesis para tenerlas unidas"
+text # Imprime el valor de "text"
 
+prefijo = 'Py' # Guarda la variable 'prefijo' con el valor 'Py'.
+prefijo 'thon' # Guarda la variable 'prefijo' con el valor 'thon'. #????
+('un' * 3) 'ium' #?????
 
+prefijo + 'thon' # Muestra la palabra Python entre comillas sencillas.
 
+nombre = "Ada" # Guarda la variable "nombre" con el valor "Ada"
+apellido = "Lovelace" # Guarda la variable "apellido" con el valor "Lovelace"
 
-
-
-
-
+print(nombre.upper()) # Imprime "nombre" con letras mayúsculas "ADA"
+print(nombre.lower()) # Imprime "nombre" con letra minúsculas "ada"
+print(nombre.isalnum()) # Imprime "True", ya que la variable tiene caracteres alfanumérico.
+print(nombre.isalpha()) # Imprime "True", ya que la variable tiene caracteres alfabeticos
+print(nombre.islower()) # Imprime "False", ya que no todos los caracteres de la variable son minúsculas.
+print(nombre.isnumeric()) # Imprime "False", ya que la variable no es numérica.
+print(nombre.isspace()) # Imprime "False", ya que la variable no contiene solamente espacios.
+print(nombre.istitle()) # Imprime "True", ya que la varibale contiene caracteres en mayusculas y en minusculas.
+asignatura = "Taller De Herramientas Computacionales"
+print(asignatura.istitle()) # Imprime "True", ya que la variable contiene caracteres tanto en minusculas como en mayusculas.
+print(asignatura.isupper()) # Imprime "False", ya que no todos los caracteres de la varibale son mayusculas.
 
 numero = "1024" # Guarda "numero" como el valor de 1024
 vocales = "aeiou" # Guarda "vocales" como "aeiou"
 print(numero.isnumeric()) # Analiza si la variable "numero" tiene valor numérico e imprime el resultado (True)
 print(vocales.isnumeric()) # Analiza si la variable "vocales" tiene valor numérico e imprime el resultado (False)
 
-pelicula = "2001: UNA MENTE BRILLANTE" #
-libro = "Cinco Ecuaciones Que CAmbiaron Al Mundo" #
-poema = "nadie en oro se convertirá:" #
+pelicula = "2001: UNA MENTE BRILLANTE" # Guarda "pelicula" como "2001: UNA MENTE BRILLANTE"
+libro = "Cinco Ecuaciones Que Cambiaron Al Mundo" # Guarda "Libro" como "Cinco Ecuaciones Que Cambiaron Al Mundo"
+poema = "nadie en oro se convertirá:" # Guarda "poema" como "nadie en oro se convertirá"
+print(pelicula.islower()) # Analiza si la variable "pelicula" está escrita con minúsculas e imprime el resultado (False)
+print(pelicula.isupper()) # Analiza si la variable "pelicula" está escrita con mayúsculas e imprime el resultado (True)
 
-print(pelicula.islower()) #
-print(pelicula.isupper()) #
+print(libro.istitle()) # Analiza si la variable "libro" está escrita en el formato de título imprime el resultado (True)
+print(libro.isupper()) # Analiza si la variable "libro" está escrita con mayúsculas e imprime el resultado (False)
 
-print(libro.istitle()) #
-print(libro.isupper()) #
+print(poema.istitle()) # Analiza si la variable "poema" está escrita en el formato de título imprime el resultado (False)
+print(poema.islower()) # Analiza si la variable "libro" está escrita con miníusculas e imprime el resultado (True)
 
-print(poema.istitle()) #
-print(poema.islower()) #
+cadena = "Linux es un kernel." # Guarda la variable "Cadena" como "Linux es un kernel."
+" ".join(cadena) # Agrega un espacio entre cada caractér, incluyendo espacios
+print(cadena) # Muestra la variable "Cadena"
+print(" ".join(cadena)) # Muestra la variable "Cadena" con espacios añadidos
 
-cadena = "Linux es un kernel." #
-" ".join(cadena) #
-print(cadena) #
-print(" ".join(cadena)) #
-
-print("".join(reversed(cadena))) #
+print("".join(reversed(cadena))) # Muestra la variable "Cadena" escrita de derecha a izquierda"
 
 # https://docs.python.org/3/tutorial/introduction.html#strings
 # https://www.digitalocean.com/community/tutorials/an-introduction-to-string-functions-in-python-3
