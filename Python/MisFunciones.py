5ba11bb0d2a950666cd74b3cdc0426ba71fa7aa1
Maximiliano Proaño Bernal <maxpbernal@ciencias.unam.mx>
2019-10-10 08:17:58 -0500

    Se agregó MisFunciones.py

---
+++ Python/MisFunciones.py
@@ -0,0 +1,88 @@
+"""
+Funciones definidas por el usuario
+"""
+def vabs(x):
+    '''Regresa el valor absoluto del numero x.
+Regresa  x si x es mayor que cero
+Regresa -x si x es menor que cero
+'''
+    return(resultado)
+
+def signo(x):
+    '''Regresa el signo del numero x.
+Regresa  1 si x es positivo
+Regresa  0 si x es cero
+Regresa -1 si x es negativo
+'''
+    return(resultado)
+
+def multiplica(a,b):
+    '''Regresa el resultado de multiplicar
+    el numero a por el numero b
+'''
+    return(resultado)
+
+def elMayor(a,b):
+    '''Regresa el mayor de los numeros: a, b
+'''
+    return(resultado)
+
+def elMenor(a,b):
+    '''Regresa el menor de los numeros: a, b
+'''
+    return(resultado)
+
+def rectangular(x):
+    '''Regresa la evaluacion de x en
+la funcion definida como:
+Regresa 0   si |x| > 1/2
+Regresa 1/2 si |x| = 1/2
+Regresa 1   si |x| < 1/2
+'''
+    return(resultado)
+
+def identidad(x):
+    '''regresa la identidad del valor x
+'''
+    return(resultado)
+
+def rampa(x):
+    '''Regresa 0 si x < 0
+Regresa x si x >= 0
+'''
+    return(resultado)
+
+def parte_entera(x):
+    ''' Regresa la parte entera de x,
+por ejemplo si x = 9.23 regresa 9
+'''
+    return(resultado)
+
+def enteroMayor(x):
+    '''Regresa el entero mayor de x
+ej, si x = 7.1 regresaria 8
+'''
+    return(resultado)
+
+def enteroMenor(x):
+    '''Regresa el entero meno de x
+ej, si x = 7.1 regresaria 7
+'''
+    return(resultado)
+
+def parte_fraccionaria(x):
+    ''' Regresa la parte fraccionaria de x definida como
+x - enteroMenor(x)
+ej, si x = 9.23 regresaria:
+    9.23 - floor(9.23) = 9.23 - 9
+regresa 0.23
+ej, si x = -7.26 regresaria:
+    -7.26 - floor(-7.26) = -7.26 - (-8) = -7.26 + 8
+regresa 0.74
+'''
+    return(resultado)
+
+def ulam(n):
+    '''Regresa n/2 si n es par
+Regresa 3*n + 1 si n es impar
+'''
