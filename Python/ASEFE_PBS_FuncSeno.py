f82bc72a0f16a7147ead1c127fbb9b3a541f68a9
Maximiliano Proaño Bernal <maxpbernal@ciencias.unam.mx>
2019-11-11 23:04:37 -0600

    Se agregaron ASEFE_PBS_FuncSeno, ASEFE_PBS_Seno y Pasos para compilar el archivo Seno.tex

---
+++ Python/ASEFE_PBS_FuncSeno.py
@@ -0,0 +1,28 @@
+# Maximiliano Proaño Bernal
+# 10/11/19
+# 12/11/19
+# Python 3.7.3
+# Calcula el seno
+
+from math import sin, pi, sqrt
+def seno(alfa):
+    """ Calcula el seno de \
+el ángulo alfa"""
+    b = alfa/(2**5)
+    for i in range(5):
+        c = 2*sin(b)*sqrt(1-sin(b)**2)
+        b = 2*b
+    resultado = c
+    return(resultado)
+    
+if __name__ == "__main__":
+    seno(.5)
+    print("Este bloque se ejecuta si el programa \
+es llamado desde IDLE, la variable __name__ tiene \
+almacenada la cadena '__main__' ")
+    print(__name__)
+else:
+    print("Si el archivo se utiliza como modulo,\
+ es decir se importa, la variable __name__ contiene\
+el nombre nombre del archivo")
+    print(__name__)
---
+++ Python/ASEFE_PBS_Seno.py
@@ -0,0 +1,43 @@
+# Maximiliano Proaño Bernal
+# 11/11/19
+# 12/11/19
+# Python 3.7.3
+# Calcula el seno del ángulo a
+
+from math import pi, sin, sqrt
+a = 0.1
+b = a/(2**5)
+print("""
+Para a = 0.1:""")
+for i in range(5):
+    c = 2*sin(b)*sqrt(1-sin(b)**2)
+    print("Paso %.0f: sin(%.5f) = %.5f"%(i+1,2*b,c))
+    b = 2*b
+
+a = 0.5
+b = a/(2**5)
+print("""
+Para a = 0.5:""")
+for i in range(5):
+    c = 2*sin(b)*sqrt(1-sin(b)**2)
+    print("Paso %.0f: sin(%.5f) = %.5f"%(i+1,2*b,c))
+    b = 2*b
+
+a = 1.5
+b = a/(2**5)
+print("""
+Para a = 1.5:""")
+for i in range(5):
+    c = 2*sin(b)*sqrt(1-sin(b)**2)
+    print("Paso %.0f: sin(%.5f) = %.5f"%(i+1,2*b,c))
+    b = 2*b
+
+a = pi
+b = a/(2**5)
+print("""
+Para a = pi:""")
+for i in range(5):
+    c = 2*sin(b)*sqrt(1-sin(b)**2)
+    print("Paso %.0f: sin(%.5f) = %.5f"%(i+1,2*b,c))
+    b = 2*b
+
